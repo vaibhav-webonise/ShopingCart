8285a29c307ec173ea00a88a206c23c9869b918c
vaibhav-webonise <vaibhav.anasune@weboniselab.com>
2019-11-08 16:29:13 +0530

    shoppingcart app

---
+++ src/productdata.js
@@ -0,0 +1,10 @@
+ const initState ={
+    products:[
+    { productId: 1, productName: 'MI earphones ', quantity: 0 },
+    { productId: 2, productName: 'Fastrack watch', quantity: 0 },
+    { productId: 3, productName: 'Louis vaten sunglasses', quantity: 0 }
+    ],
+    addedProducts:[],
+    productCount:0,
+ }
+export default initState
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+import {combineReducers} from 'redux'
+import productReducer from './reducer/productReducer'
+  export default combineReducers({
+      productReducer
+  })
